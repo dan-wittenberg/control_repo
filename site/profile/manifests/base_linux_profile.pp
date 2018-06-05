8d1680883864975bf26cd8730fef6f594f9e37cb
Daniel Wittenberg <dan.wittenberg@thinkahead.com>
2018-06-05 11:39:57 -0500

    Add comments and fix OS

--- site/profile/manifests/base_linux_profile.pp
+++ site/profile/manifests/base_linux_profile.pp
@@ -3,6 +3,9 @@ class profile::base_linux_profile {
 
   #the base profile should include component modules that will be on all nodes
   include ::base_linux
+
+  # This will add custom facts to show if the host is vulnerable and tasks
+  # to help with remediation
   include ::meltdown::linux
 
 }
--- site/profile/manifests/base_windows_profile.pp
+++ site/profile/manifests/base_windows_profile.pp
@@ -3,6 +3,9 @@ class profile::base_windows_profile {
 
   #the base profile should include component modules that will be on all nodes
   include ::base_windows
-  include ::meltdown::linux
+
+  # This will add custom facts to show if the host is vulnerable and tasks
+  # to help with remediation
+  include ::meltdown::windows
 
 }
